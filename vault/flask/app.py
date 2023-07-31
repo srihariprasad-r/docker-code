1caef16a42045cdf86997ef6805e666b44b8c798
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-07-31 23:33:47 +0530

    updated code

--- vault/flask/app.py
+++ vault/flask/app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template, request
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from src.dev.demo import Demo
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+conf = Demo.get_config_entries()
+
 @app.route("/")
 def home():
-    print(request.method)
-    if request.method == 'GET': print(request.args)
+    params = {
+        'filetype': request.args.filetype,
+        'filepath': '/src/files',
+        'csvdelimiter':  ','
+    }
+
+    dbc = Demo(url=conf['VAULT']['Address'], token=conf['VAULT']['Token'], namespace=conf['VAULT']['Namespace'], **params)
     return render_template('index.html')
 
 
