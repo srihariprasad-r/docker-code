71b9ea0969732270e1707a9dd3042e851f8a94ac
srihariprasad-r <59530606+srihariprasad-r@users.noreply.github.com>
2023-08-18 17:33:33 +0530

    removed few files

--- vault/bootup-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sh start_vault.sh
-sh docker-commands.sh
-sh copy-files.sh
--- vault/copy-files.sh
+++
@@ -1,8 +0,0 @@
-cname=$(docker inspect --format="{{.Id}}" vault-demo)
-docker cp ./config/vault-config.json vault-demo:/vault/config/vault-config.json
-docker cp ./database-setup.sh vault-demo:/vault/database-setup.sh
-docker cp ./transit-secret-engine.sh vault-demo:/vault/transit-secret-engine.sh
-docker cp ./transform-fpe-api-setup.sh vault-demo:/vault/transform-fpe-api-setup.sh
-docker cp ./transform-mask-api-setup.sh vault-demo:/vault/transform-mask-api-setup.sh
-# docker exec vault-demo /bin/sh -c './vault/database-setup.sh'
-# docker exec vault-demo /bin/sh -c './vault/transit-secret-engine.sh'
